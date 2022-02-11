13e97a6bcd31e75d0f0772e18dc0df86d6c00209
Radovan Bast <bast@users.noreply.github.com>
2022-02-11 15:26:40 +0100

    extract also title

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ from lxml import etree
 from datetime import datetime
 import hashlib
 from google.cloud import firestore
+from bs4 import BeautifulSoup
 
 
 def extract_text(url: str, language: str, session) -> (list[str], str):
@@ -13,11 +14,13 @@ def extract_text(url: str, language: str, session) -> (list[str], str):
     paragraphs = []
     t0 = time.time()
     response = session.get(url)
-    page = response.text.encode("utf-8")
+    html = response.text.encode("utf-8")
     print("time spent in requests:", time.time() - t0)
+    soup = BeautifulSoup(html, "html.parser")
+    title = soup.find("title")
 
     t0 = time.time()
-    for paragraph in justext.justext(page, justext.get_stoplist(language)):
+    for paragraph in justext.justext(html, justext.get_stoplist(language)):
         if paragraph.class_type == "good":
             if paragraph.heading:
                 headings.append(paragraph.text)
@@ -25,20 +28,16 @@ def extract_text(url: str, language: str, session) -> (list[str], str):
     print("time spent in justext:", time.time() - t0)
 
     # returning response.url since url might be redirected
-    return response.url, headings, "\n".join(paragraphs)
+    return response.url, title.string, headings, "\n".join(paragraphs), html
 
 
 def test_extract_test():
     url = "https://w.wiki/U"
     language = "English"
     session = requests.Session()
-    final_url, headings, text = extract_text(url, language, session)
+    final_url, title, headings, text, html = extract_text(url, language, session)
     assert final_url == "https://en.wikipedia.org/wiki/URL_shortening"
     assert "URL shortening is a technique on the World Wide Web" in text
-
-
-def get_timestamp() -> str:
-    return datetime.now().astimezone().replace(microsecond=0).isoformat()
 
 
 def hash_url(url: str) -> str:
@@ -58,25 +57,22 @@ def get_urls(sitemap: str) -> list[str]:
     return urls
 
 
-def print_data(url: str, language: str, session):
-    _, headings, text = extract_text(url, language, session)
-    timestamp = get_timestamp()
-    document_id = hash_url(url)
-    # print(f"\nheadings: {headings}")
-    # print(f"\ntext: {text}")
-    # print(f"\ntimestamp: {timestamp}")
-    # print(f"\ndocument_id: {document_id}")
-
-
 def extract_and_write_to_db(url: str, language: str, session):
-    _, headings, text = extract_text(url, language, session)
-    timestamp = get_timestamp()
-    document_id = hash_url(url)
+    final_url, title, headings, text, html = extract_text(url, language, session)
+    unix_time = int(time.time())
+    document_id = f"{hash_url(url)}-{unix_time}"
+
+    d = {}
+    d["url"] = final_url
+    d["timestamp"] = unix_time
+    d["title"] = title
+    d["headings"] = headings
+    d["text"] = text
+    d["html"] = html
 
     db = firestore.Client()
-
     doc_ref = db.collection("articles").document(document_id)
-    doc_ref.set({"timestamp": timestamp, "headings": headings, "text": text})
+    doc_ref.set(d)
 
 
 if __name__ == "__main__":
@@ -84,5 +80,4 @@ if __name__ == "__main__":
     language = "Norwegian_Bokmal"
     session = requests.Session()
     for url in tqdm(urls):
-        # extract_and_write_to_db(url, language, session)
-        print_data(url, language, session)
+        extract_and_write_to_db(url, language, session)
