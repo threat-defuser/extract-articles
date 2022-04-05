ff266325dc07353ff3fa0dbfe020e51b7cea77fc
Radovan Bast <bast@users.noreply.github.com>
2022-04-05 12:24:20 +0200

    switch to sqlite

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+import time
+from contextlib import closing
+
+
+def make_sure_table_exists(db_file_name: str):
+    with closing(sqlite3.connect(db_file_name)) as connection:
+        with closing(connection.cursor()) as cursor:
+            cursor.execute(
+                """CREATE TABLE IF NOT EXISTS
+                     articles(
+                       timestamp INTEGER,
+                       url TEXT,
+                       title TEXT,
+                       text TEXT,
+                       html TEXT
+                       )
+                """
+            )
+
+
+def save_entry(db_file_name: str, url: str, title: str, text: str, html: str):
+    with closing(sqlite3.connect(db_file_name)) as connection:
+        with closing(connection.cursor()) as cursor:
+            unix_time = int(time.time())
+            cursor.execute(
+                "INSERT INTO articles VALUES (?, ?, ?, ?, ?)",
+                (unix_time, url, title, text, html),
+            )
+
+
+def read_entries(db_file_name: str):
+    with closing(sqlite3.connect(db_file_name)) as connection:
+        with closing(connection.cursor()) as cursor:
+            rows = cursor.execute("SELECT * FROM articles").fetchall()
+            return rows
--- example.py
+++ example.py
@@ -1,83 +1,19 @@
-from tqdm import tqdm
-import justext
-import time
 import requests
-from lxml import etree
-from datetime import datetime
-import hashlib
-from google.cloud import firestore
-from bs4 import BeautifulSoup
+
+from extract_plaintext import extract
+from db import make_sure_table_exists, save_entry, read_entries
 
 
-def extract(url: str, language: str, session) -> (list[str], str):
-    headings = []
-    paragraphs = []
-    t0 = time.time()
-    response = session.get(url)
-    html = response.text.encode("utf-8")
-    print("time spent in requests:", time.time() - t0)
-    soup = BeautifulSoup(html, "html.parser")
-    title = soup.find("title")
-
-    t0 = time.time()
-    for paragraph in justext.justext(html, justext.get_stoplist(language)):
-        if paragraph.class_type == "good":
-            if paragraph.heading:
-                headings.append(paragraph.text)
-            paragraphs.append(paragraph.text)
-    print("time spent in justext:", time.time() - t0)
-
-    # returning response.url since url might be redirected
-    return response.url, title.string, headings, "\n".join(paragraphs), html
-
-
-def test_extract():
-    url = "https://w.wiki/U"
-    language = "English"
+def extract_and_save(db_file_name: str, url_language_tuples: tuple[str, str]):
+    make_sure_table_exists(db_file_name)
     session = requests.Session()
-    final_url, title, headings, text, html = extract(url, language, session)
-    assert final_url == "https://en.wikipedia.org/wiki/URL_shortening"
-    assert "URL shortening is a technique on the World Wide Web" in text
+    for (url, language) in url_language_tuples:
+        final_url, title, _headings, text, html = extract(url, language, session)
+        save_entry(db_file_name, final_url, title, text, html)
 
 
-def hash_url(url: str) -> str:
-    return hashlib.sha256(url.encode("utf-8")).hexdigest()
-
-
-def get_urls(sitemap: str) -> list[str]:
-    urls = []
-    r = requests.get(sitemap)
-    root = etree.fromstring(r.content)
-    for sitemap in root:
-        children = sitemap.getchildren()
-        if len(children) == 2:
-            url = children[0].text
-            if "utenriks" in url:  # FIXME
-                urls.append(url)
-    return urls
-
-
-def extract_and_write_to_db(url: str, language: str, session):
-    final_url, title, headings, text, html = extract(url, language, session)
-    unix_time = int(time.time())
-    document_id = f"{hash_url(url)}-{unix_time}"
-
-    d = {}
-    d["url"] = final_url
-    d["timestamp"] = unix_time
-    d["title"] = title
-    d["headings"] = headings
-    d["text"] = text
-    d["html"] = html
-
-    db = firestore.Client()
-    doc_ref = db.collection("articles").document(document_id)
-    doc_ref.set(d)
-
-
-if __name__ == "__main__":
-    urls = get_urls("https://www.lykten.no/sitemap.xml")
-    language = "Norwegian_Bokmal"
-    session = requests.Session()
-    for url in tqdm(urls):
-        extract_and_write_to_db(url, language, session)
+def test_extract_and_save():
+    db_file_name = "articles.db"
+    url_language_tuples = [("https://w.wiki/U", "English")]
+    extract_and_save(db_file_name, url_language_tuples)
+    rows = read_entries(db_file_name)
